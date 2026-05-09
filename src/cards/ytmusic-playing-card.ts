95b463b373b3c28c55fcf77926cdaad1a625ff0b
cash83 <62354219+cash83@users.noreply.github.com>
2026-05-09 06:29:53 +0200

    Fix volume initialization and update parameters for service call

--- src/cards/ytmusic-playing-card.ts
+++ src/cards/ytmusic-playing-card.ts
@@ -75,8 +75,7 @@ export class YTMusicPlayingCard extends LitElement {
     private _rootItems: any[] = [];
     private _homeItems: any[] = [];
     private _rootLoaded = false;
-    private _volumeInitialized = false;
-
+    private _volumeInitialized = false;   
     static getConfigElement() {}
 
     static getStubConfig() {
@@ -399,7 +398,7 @@ export class YTMusicPlayingCard extends LitElement {
                                 this._hass.callService("ytube_music_player", "call_method", {
                                     entity_id: this._config.entity_id,
                                     command: "goto_track",
-                                    parameters: i,
+                                    parameters: i + 1,
                                 });
                             }}>
                             ${thumb
